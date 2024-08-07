8ee6b7af3edb39ec98a4f801818a22b61a1bfd51
양세윤 <dosy13@naver.com>
2024-08-07 23:05:08 +0900

    * 글 리스트 가져오기 * 페이징 추가

--- server/routes/main.js
+++ server/routes/main.js
@@ -3,13 +3,53 @@ const router = express.Router();
 const Post = require('../models/Post');
 
 //Routes
-router.get('', (req, res) => {
-    const locals = {
-        title: "NodeJs Blog",
-        description: "Simple Blog created with NodeJs, Express & MongoDb."
+router.get('', async (req, res) => {
+    
+
+    try {
+        const locals = {
+            title: "NodeJs Blog",
+            description: "Simple Blog created with NodeJs, Express & MongoDb."
+        }
+
+        // paging
+        let perPage = 10;
+        let page = req.query.page || 1;
+
+        const data = await Post.aggregate([ { $sort: {createdAt: -1}}])
+        .skip(perPage * page - perPage)
+        .limit(perPage)
+        .exec();
+
+        const count = await Post.countDocuments({});
+        const nextPage = parseInt(page) + 1;
+        const hasNextPage = nextPage <= Math.ceil(count / perPage);
+
+        res.render('index', {
+            locals,
+            data,
+            current: page,
+            nextPage: hasNextPage ? nextPage : null
+        });
+    } catch (error){
+        console.log('error: ', error);
     }
-    res.render('index', {locals});
 });
+
+//Routes
+// router.get('', async (req, res) => {
+//     const locals = {
+//         title: "NodeJs Blog",
+//         description: "Simple Blog created with NodeJs, Express & MongoDb."
+//     }
+
+//     try {
+//         const data = await Post.find();
+//         res.render('index', {locals, data});
+//     } catch (error){
+//         console.log('error: ', error);
+//     }
+// });
 
 // 더미 데이터 넣는 방법
 // function insertPostData() {
